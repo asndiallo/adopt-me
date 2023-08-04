63014dc14036cc6c9873d8eb7a5cae0da844b88a
Assane Diallo <assane@pando2.com>
2023-08-04 15:42:59 +0200

    [fetchBreedList.js, useBreedList.js] Refactor fetchBreedList and useBreedList to use react-query for breed list fetch

---
+++ src/utils/fetchBreedList.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+/**
+ * Fetches a list of breeds for a given animal from an external API.
+ * 
+ * @param {Array} queryKey - An array containing the animal name as the second element.
+ * @returns {Promise} - A promise that resolves to the data from the API response if the request is successful.
+ * @throws {Error} - If there is an error during the API call, an error with a specific message will be thrown.
+ */
+const fetchBreedList = async ({ queryKey }) => {
+    const animal = queryKey[1];
+
+    if (!animal) {
+        throw new Error('No animal provided');
+    }
+
+    try {
+        const response = await axios.get(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
+        return response.data;
+    } catch (error) {
+        const errorMessage = error.response ? error.response.data : error.message;
+        throw new Error(`Failed to fetch breeds for ${animal}: ${errorMessage}`);
+    }
+};
+
+export default fetchBreedList;
+
--- src/utils/useBreedList.js
+++ src/utils/useBreedList.js
@@ -1,7 +1,5 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
-const localCache = {};
+import { useQuery } from '@tanstack/react-query';
+import fetchBreedList from './fetchBreedList';
 
 /**
  * Custom React hook to fetch and manage a list of animal breeds.
@@ -9,48 +7,6 @@ const localCache = {};
  * @returns {Array} - An array containing the breed list and the status of the data fetching process.
  */
 export default function useBreedList(animal) {
-    const [breedList, setBreedList] = useState([]);
-    const [status, setStatus] = useState("unloaded");
-
-    useEffect(() => {
-        let isMounted = true;
-
-        const requestBreedList = async () => {
-            setBreedList([]);
-            setStatus("loading");
-
-            try {
-                if (localCache[animal]) {
-                    setBreedList(localCache[animal]);
-                    setStatus("loaded");
-                } else {
-                    const response = await axios.get(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
-                    const data = response.data;
-
-                    if (isMounted) {
-                        setBreedList(data.breeds || []);
-                        setStatus("loaded");
-                        localCache[animal] = data.breeds || [];
-                    }
-                }
-            } catch (error) {
-                console.error("Error fetching breed list:", error);
-                setStatus("error");
-            }
-        };
-
-        if (!animal) {
-            setBreedList([]);
-        } else if (localCache[animal]) {
-            setBreedList(localCache[animal]);
-        } else {
-            requestBreedList();
-        }
-
-        return () => {
-            isMounted = false;
-        };
-    }, [animal]);
-
-    return [breedList, status];
+    const { data, status } = useQuery(['breeds', animal], fetchBreedList);
+    return [data?.breeds ?? [], status];
 }
