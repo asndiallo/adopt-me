16d2fa6625e65fe0aa7454c7e47cb008ae870b8d
Assane Diallo <assane@pando2.com>
2023-08-04 15:28:34 +0200

    [React Query] Enhance App component to use react-query for "Pet" detail data fetching

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,8 +1,48 @@
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import fetchPet from '../utils/fetchPet';
 
+/**
+ * Renders the details of a specific pet based on the ID parameter passed through the URL.
+ * Uses the 'useParams' hook to retrieve the ID and the 'useQuery' hook to fetch the pet data from an API endpoint.
+ * Handles any errors that may occur during the data fetching process.
+ *
+ * @returns {JSX.Element} The rendered component displaying the pet details or an error message.
+ */
 const Details = () => {
   const { id } = useParams();
-  return <h2>{id}</h2>;
+  const { data, isLoading, isError } = useQuery(['details', id], fetchPet);
+
+  if (isError) {
+    return (
+      <div className="details">
+        <h2>Something went wrong.</h2>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const { name, animal, breed, description, city, state } = data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{name}</h1>
+        <h2>
+          {animal} — {breed} -- {city}, {state}
+        </h2>
+        <button>Adopt Me</button>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
---
+++ src/utils/fetchPet.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+/**
+ * Fetches pet data from an external API based on the provided pet ID.
+ * @param {Array} queryKey - An array containing the query key and pet ID.
+ * @returns {Promise} - A promise that resolves to the fetched pet data.
+ * @throws {Error} - If the API request fails or the response data is empty.
+ */
+const fetchPet = async ({ queryKey }) => {
+    const id = queryKey[1];
+
+    try {
+        const response = await axios.get(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+        return response.data;
+    } catch (error) {
+        const errorMessage = error.response ? error.response.data : error.message;
+        throw new Error(`details/${id} fetch not ok: ${errorMessage}`);
+    }
+};
+
+export default fetchPet;
+
