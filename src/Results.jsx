db6308be6599eef6661407b5602593b9b4990fcb
Assane Diallo <assane@pando2.com>
2023-08-04 11:32:00 +0200

    [Results] Update documentation for the "pets" prop, explaining its structure and purpose. This is necessary to provide clarity for developers who are working with this component.

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,5 +1,11 @@
 import Pet from './Pet';
 
+/**
+ * Renders a list of pets based on the input 'pets' array. Handles the case when no pets are found.
+ * 
+ * @param {Array} pets - An array of objects representing pets, with properties such as name, animal, breed, images, location, and id.
+ * @returns {JSX.Element} - A div with class "search" containing either a message saying "No Pets Found" or a list of 'Pet' components.
+ */
 const Results = ({ pets }) => {
   return (
     <div className="search">
