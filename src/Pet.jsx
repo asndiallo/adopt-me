0ac8e60e8dc28ba5fdcae3f7adcb899852563570
Assane Diallo <assane@pando2.com>
2023-08-04 11:33:16 +0200

    [Pet.jsx] Add comments to describe prop types and component functionality. Improves code readability and maintainability.

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,3 +1,16 @@
+/**
+ * Renders a pet card with information such as name, animal, breed, images, location, and id.
+ * Provides a link to the pet details page.
+ *
+ * @param {Object} props - The pet information.
+ * @param {string} props.name - The name of the pet.
+ * @param {string} props.animal - The type of animal.
+ * @param {string} props.breed - The breed of the pet.
+ * @param {string[]} props.images - The images of the pet.
+ * @param {string} props.location - The location of the pet.
+ * @param {string} props.id - The ID of the pet.
+ * @returns {JSX.Element} - The pet card component.
+ */
 const Pet = (props) => {
   const { name, animal, breed, images, location, id } = props;
 
