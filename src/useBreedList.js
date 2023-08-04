0b25c3e446355273f37bc1b28b3a7cadffbef0df
Assane Diallo <assane@pando2.com>
2023-08-04 12:21:55 +0200

    [useBreedList] Add fetch cancellation to prevent memory leaks

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -2,11 +2,36 @@ import { useState, useEffect } from "react";
 
 const localCache = {};
 
+/**
+ * Custom React hook to fetch and manage a list of animal breeds.
+ * @param {string} animal - The type of animal for which to fetch the breed list.
+ * @returns {Array} - An array containing the breed list and the status of the data fetching process.
+ */
 export default function useBreedList(animal) {
     const [breedList, setBreedList] = useState([]);
     const [status, setStatus] = useState("unloaded");
 
     useEffect(() => {
+        let isMounted = true;
+
+        const requestBreedList = async () => {
+            setBreedList([]);
+            setStatus("loading");
+
+            try {
+                const res = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
+                const json = await res.json();
+
+                if (isMounted) {
+                    setBreedList(json.breeds || []);
+                    setStatus("loaded");
+                }
+            } catch (error) {
+                console.error("Error fetching breed list:", error);
+                setStatus("error");
+            }
+        };
+
         if (!animal) {
             setBreedList([]);
         } else if (localCache[animal]) {
@@ -15,17 +40,9 @@ export default function useBreedList(animal) {
             requestBreedList();
         }
 
-        async function requestBreedList() {
-            setBreedList([]);
-            setStatus("loading");
-            const res = await fetch(
-                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            );
-            const json = await res.json();
-            localCache[animal] = json.breeds || [];
-            setBreedList(localCache[animal]);
-            setStatus("loaded");
-        }
+        return () => {
+            isMounted = false;
+        };
     }, [animal]);
 
     return [breedList, status];
