72f340f7ffbbccd08ce02e3ab03ff66546d95b02
Assane Diallo <assane@pando2.com>
2023-08-04 22:22:11 +0200

    Refactor modal component to use useRef instead of importing React and using its shortcut for creating a ref. This change allows the component to have better performance by avoiding unnecessary re-renders and updates unnecessary dependencies.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,29 +1,28 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 /**
- * Modal component that renders its children inside a new DOM element appended to the 'modal' root element.
+ * Modal component that renders its children inside a new DOM element appended to the 'modal-root' element.
  * @param {Object} props - The component props.
  * @param {React.ReactNode} props.children - The children to be rendered inside the modal.
  * @returns {React.ReactPortal} The modal component.
  */
 const Modal = ({ children }) => {
-  const modalRoot = useRef(document.getElementById('modal'));
-
-  const elRef = useRef(document.createElement('div'));
+  const modalRootRef = useRef(document.getElementById('modal'));
+  const modalRef = useRef(document.createElement('div'));
 
   useEffect(() => {
-    const { current: el } = elRef;
-    const { current: modal } = modalRoot;
+    const modalRoot = modalRootRef.current;
+    const modal = modalRef.current;
 
-    modal.appendChild(el);
+    modalRoot.appendChild(modal);
 
     return () => {
-      modal.removeChild(el);
+      modalRoot.removeChild(modal);
     };
   }, []);
 
-  return createPortal(<div>{children}</div>, elRef.current);
+  return createPortal(<div>{children}</div>, modalRef.current);
 };
 
 export default Modal;
